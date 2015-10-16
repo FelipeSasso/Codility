703e813bcac9b85c9f77882992e103efe4628d29
Felipe Coral Sasso <felipecsasso@gmail.com>
2015-10-16 16:13:14 -0300

    PermMissingElem

---
+++ Lesson1/PermMissingElem.py
@@ -0,0 +1,24 @@
+tdout for debugging purposes, e.g.
+# print "this is a debug message"
+
+def solution(A):
+    # write your code in Python 2.7
+    A.sort()
+    arraySize = len(A)
+    
+    if arraySize == 0:
+        return 1
+    elif arraySize == 1:
+        if A[0] == 1:
+            return 2
+        else:
+            return 1
+    
+    if arraySize == A[arraySize - 1]:
+        return arraySize + 1
+    
+    for i in range(arraySize):       
+        
+        if i + 1 != A[i]:
+            return i + 1
+
